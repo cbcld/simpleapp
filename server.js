30c3c9cdef707d3fd9dd9f119f00d161d5436091
Singh <jayasingh6@deloitte.com>
2019-09-17 12:39:04 +0530

    api code

--- server.js
+++ server.js
@@ -18,3 +18,34 @@ res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 
 
 console.log('running node js');
+
+
+var Request = require("request");
+var user = 'LLYMME\C291410'
+var pass = 'Johnwick16'
+var result = [];
+var response = [];
+
+let options = {
+        url: "https://eicinfdev.am.lilly.com:9185/access/2/catalog/data/objects",
+        //url:"https://api.bitbucket.org/2.0/hook_events",
+        method: "GET",
+        proxy:"http://proxy.gtm.lilly.com:9000",
+        headers: {'Accept':'*/*','Content-Type': '*/*'},
+        auth: { username: user, password: pass },
+        qs: {
+            q: "EDB_Raw",
+            offset: 0,
+            pageSize: 20,
+              
+        }
+        
+        
+   
+        
+    }
+    
+    Request(options,(err,res2,body)  => {
+        console.log(body);
+        
+    });
