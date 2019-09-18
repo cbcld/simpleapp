701cfd97a1459745cafc5ab4f3b77bc404e8bdd0
Singh <jayasingh6@deloitte.com>
2019-09-18 17:16:40 +0530

    mail

--- server.js
+++ server.js
@@ -21,22 +21,22 @@ res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 console.log('running node js');
 
 var transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com", // hostname
+    host: "smtp-gmail.com", // hostname
     secureConnection: false, // TLS requires secureConnection to be false
     port: 587, // port for secure SMTP
     tls: {
        ciphers:'SSLv3'
     },
     auth: {
-        user: 'jayasingh6@deloitte.com',
-        pass: 'Demanding@21'
+        user: 'jaya21591@gmail.com',
+        pass: 'Highdemand@21'
     }
 });
 
 // setup e-mail data, even with unicode symbols
 var mailOptions = {
-    from: 'jayasingh6@deloitte.com', // sender address (who sends)
-    to: 'jayasingh6@deloitte.com, bbaraiya@deloitte.com', // list of receivers (who receives)
+    from: 'jaya21591@gmail.com', // sender address (who sends)
+    to: 'jaya21591@gmail.com', // list of receivers (who receives)
     subject: 'Hello ', // Subject line
     text: 'Hello world ', // plaintext body
     html: '<b>Hello world </b><br> This is the first email sent with Nodemailer in Node.js' // html body
